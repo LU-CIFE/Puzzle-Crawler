5aa8d00ff0f17db6554f32868d3dc9bb6251f9f7
LUSIFE <62038653+LU-CIFE@users.noreply.github.com>
2022-05-16 16:38:20 -0600

    Update puzzle_crawler_script.js

--- puzzle_crawler_script.js
+++ puzzle_crawler_script.js
@@ -1225,10 +1225,12 @@
 								shake_effect("health_container");
 							}
 						} else if (cursor_pos == 78) {
-							cash --;
-							recovery_amount ++;
-							shake_effect("cash_container");
-							shake_effect("recovery_container");
+							if (cash - 1 >= 0) {
+								cash--;
+								recovery ++;
+								shake_effect("cash_container");
+								shake_effect("recovery_container");
+							}
 						}
 						
 						document.getElementById("recovery_container").innerHTML = "Recovery:: " + recovery;
